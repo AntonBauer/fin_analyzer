575aac57e3608fe72631a31b56d4be6c4fb40a22
Anton Bauer <anton.bauer2@generic.de>
2025-01-16 13:57:40 +0100

    add load ing data endpoint

--- src/FinAnalyzer.Api/UseCases/LoadIngData/WebApplicationExtensions.cs
+++ src/FinAnalyzer.Api/UseCases/LoadIngData/WebApplicationExtensions.cs
@@ -1,9 +1,21 @@
+using FinAnalyzer.IngData;
+using Microsoft.AspNetCore.Mvc;
+
 namespace FinAnalyzer.Api.UseCases.LoadIngData;
 
 public static class WebApplicationExtensions
 {
     public static WebApplication AddLoadIngDataEndpoints(this WebApplication app)
     {
+        app.MapPost("/load-ing-data", async ([FromServices] IIngDataParser dataParser,
+                                             [FromForm] IFormFile transactionsFileStream,
+                                             CancellationToken cancellationToken) =>
+        {
+            var transactions = await dataParser.ParseTransactions(transactionsFileStream.OpenReadStream(),
+                                                                   cancellationToken);
+            return Results.Ok(transactions);
+        });
+
         return app;
     }
 }

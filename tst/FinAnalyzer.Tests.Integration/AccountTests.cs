b3a4258d01a364695eee1b9c424a70eeb99092e7
Anton Bauer <anton.bauer2@generic.de>
2025-01-21 13:19:28 +0100

    cleanup tests

--- tst/FinAnalyzer.Tests.Integration/AccountTests.cs
+++ tst/FinAnalyzer.Tests.Integration/AccountTests.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net;
 using System.Net.Http.Json;
-using FinAnalyzer.Api.Dtos;
 using FinAnalyzer.Domain.Models;
 
 namespace FinAnalyzer.Tests.Integration;
@@ -9,7 +8,6 @@ namespace FinAnalyzer.Tests.Integration;
 internal sealed class AccountTests : IntegrationTestBase
 {
     private string? _accountLocation;
-    private string? _categoryLocation;
 
     [Test, Order(0)]
     public async Task Should_upload_ing_data()
@@ -75,49 +73,6 @@ internal sealed class AccountTests : IntegrationTestBase
             Assert.That(transactions, Has.Length.EqualTo(10));
         });
     }
-
-    [Test, Order(4)]
-    public async Task Should_create_category()
-    {
-        // Act
-        var response = await Client.PostAsJsonAsync("/categories", new CreateCategoryDto { Name = "Test category" });
-
-        // Assert
-        Assert.Multiple(() =>
-        {
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
-            Assert.That(response.Headers.Location, Is.Not.Null);
-        });
-
-        _categoryLocation = response.Headers.Location.ToString();
-    }
-
-    [Test, Order(5)]
-    public async Task Should_read_all_categories() {
-        // Act
-        var categories = await Client.GetFromJsonAsync<Category[]>("/categories");
-
-        // Assert
-        Assert.Multiple(() =>
-        {
-            Assert.That(categories, Is.Not.Null);
-            Assert.That(categories, Has.Length.EqualTo(1));
-        });
-     }
-
-    [Test, Order(6)]
-    public async Task Should_delete_category() {
-        // Act
-        await Client.DeleteAsync(_categoryLocation);
-        var categories = await Client.GetFromJsonAsync<Category[]>("/categories");
-
-        // Assert
-        Assert.Multiple(() =>
-        {
-            Assert.That(categories, Is.Not.Null);
-            Assert.That(categories, Is.Empty);
-        });
-     }
 
     private static async Task<MultipartFormDataContent> CreateContent(string filePath, string name)
     {

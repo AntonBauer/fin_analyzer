2a8686c6cb4b2eb749b6bfc46a6070c8e4543975
Anton Bauer <anton.bauer2@generic.de>
2025-01-23 15:35:36 +0100

    add suggesions migration

--- src/FinAnalyzer.DataAccess/Migrations/20250123143503_AddSuggestion.Designer.cs
+++ src/FinAnalyzer.DataAccess/Migrations/20250123143503_AddSuggestion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FinAnalyser.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FinAnalyser.DataAccess.Migrations
 {
     [DbContext(typeof(FinAnalyzerContext))]
-    partial class FinAnalyzerContextModelSnapshot : ModelSnapshot
+    [Migration("20250123143503_AddSuggestion")]
+    partial class AddSuggestion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/FinAnalyzer.DataAccess/Migrations/20250123143503_AddSuggestion.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinAnalyser.DataAccess.Migrations;
+
+/// <inheritdoc />
+public partial class AddSuggestion : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "suggestions",
+            columns: table => new
+            {
+                id = table.Column<Guid>(type: "uuid", nullable: false),
+                transaction_id = table.Column<Guid>(type: "uuid", nullable: false),
+                suggested_category_id = table.Column<long>(type: "bigint", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("pk_suggestions", x => x.id);
+                table.ForeignKey(
+                    name: "fk_suggestions_cathegories_suggested_category_id",
+                    column: x => x.suggested_category_id,
+                    principalTable: "cathegories",
+                    principalColumn: "id");
+                table.ForeignKey(
+                    name: "fk_suggestions_transactions_transaction_id",
+                    column: x => x.transaction_id,
+                    principalTable: "transactions",
+                    principalColumn: "id",
+                    onDelete: ReferentialAction.Cascade);
+            });
+
+        migrationBuilder.CreateIndex(
+            name: "ix_suggestions_suggested_category_id",
+            table: "suggestions",
+            column: "suggested_category_id");
+
+        migrationBuilder.CreateIndex(
+            name: "ix_suggestions_transaction_id",
+            table: "suggestions",
+            column: "transaction_id");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "suggestions");
+    }
+}

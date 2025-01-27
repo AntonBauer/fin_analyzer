f6992b57ec57f77c0e1615a3a70ba1fcd9cb47ad
Anton Bauer <anton.bauer2@generic.de>
2025-01-27 11:35:09 +0100

    fix button reference

--- frontend/src/app.config.ts
+++ frontend/src/app.config.ts
@@ -3,11 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(
+      withFetch()
+    ),
     provideAnimationsAsync(),
   ]
 };
--- frontend/src/views/app/app.component.ts
+++ frontend/src/views/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AccountService } from '../../services';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'fan-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })

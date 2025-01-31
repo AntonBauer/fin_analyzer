62bdc72c6d3d4d6ea5716c6f76fefbec54aa12f0
Anton Bauer <anton.bauer2@generic.de>
2025-01-31 10:17:42 +0100

    add cors policy

--- backend/src/FinAnalyzer.Api/Program.cs
+++ backend/src/FinAnalyzer.Api/Program.cs
@@ -9,6 +9,16 @@ builder.Services.AddOpenApi("fin-analyzer");
 builder.Services.AddDataAccess(builder.Configuration)
                 .AddUseCasesServices();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Allow frontend", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +26,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi()
        .CacheOutput();
 
+app.UseCors("Allow frontend");
 app.UseHttpsRedirection();
 app.AddUseCasesEndpoints();
 
